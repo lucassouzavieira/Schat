fdb5eefc4abd3cd428255cf834c30ae86d95b4f3
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2017-06-23 04:08:42 -0300

    Inicial

---
+++ resources/assets/js/components/ChatComposer.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+    <div class="chat-composer">
+        <input type="text" placeholder="Digite uma mensagem" v-model="messageText" @keyup.enter="sendMessage">
+        <button class="btn btn-primary" @click="sendMessage">Enviar</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                messageText: ''
+            }
+        },
+
+        methods: {
+            sendMessage(){
+                this.$emit('messagesent', {
+                    message: this.messageText,
+                    user: "Jhon Doe"
+                });
+                this.messageText='';
+            }
+        }
+    }
+</script>
+
+<style lang="css">
+    .chat-composer {
+        display: flex;
+    }
+
+    .chat-composer input {
+        flex: 1 auto;
+    }
+
+    .chat-composer button {
+        border-radius: 0
+    }
+</style>
